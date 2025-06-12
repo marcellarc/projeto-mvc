abda00136e5d3afb2f061e433f14bce27df605e0
marcellarc <marcellaricoy@gmail.com>
2025-06-12 11:40:26 -0300

    feat: criação do projeto

---
+++ src/main/java/com/projeto/projeto_mvc/ProjetoMvcApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.projeto_mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoMvcApplication.class, args);
+	}
+
+}
